c0f2605da52f46ef31a4884fd6e5497e9cb0a924
Douglas Simao <douglas.simao.dev@gmail.com>
2024-01-11 19:54:19 +0000

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ZXing.Net.Maui;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            cameraBarcodeReaderView.Options = new BarcodeReaderOptions
+            {
+                Formats = BarcodeFormats.OneDimensional,
+                AutoRotate = true,
+                Multiple = true
+            };
+
+            cameraBarcodeReaderView.IsTorchOn = !cameraBarcodeReaderView.IsTorchOn;
+            cameraBarcodeReaderView.CameraLocation =  CameraLocation.Rear;
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        protected void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            foreach (var barcode in e.Results)
+                Console.WriteLine($"Barcodes: {barcode.Format} -> {barcode.Value}");
+        }
+    }
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseBarcodeReader() // Make sure to add this line
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
